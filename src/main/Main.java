a22452973cce8abc5fa9cef1ada790274279cfe8
Michael V. Martin <vmartin91@gmail.com>
2017-09-28 13:14:24 -0400

    second commit

---
+++ src/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner k = new Scanner(System.in);
+    System.out.println("Press enter to be tutored.");
+    k.nextLine();
+    System.out.println("I know kung-fu.");
+    k.nextLine();
+    System.out.println("Goodbye.");
+
+  }
+
+}
