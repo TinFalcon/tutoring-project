60ff58bfd95d00b30a1dba6811f9428d336f72f4
MetalBird <vmartin91@gmail.com>
2017-10-19 13:28:51 -0400

    Fixed syntax error in LoginFrame

--- src/gui/LoginFrame.java
+++ src/gui/LoginFrame.java
@@ -13,24 +13,25 @@ import javax.swing.JOptionPane;
 
 public class LoginFrame extends JFrame {
   private List<JButton> buttons;
-  
+
   public LoginFrame() {
     super("What kind of user are you?");
     setLayout(new FlowLayout());
     ButtonHandler handler = new ButtonHandler();
-    
+
   }
-  
+
   public LinkedList<JButton> getButtons() {
     return (LinkedList<JButton>) Collections.unmodifiableList(this.buttons);
   }
 
-}
-
-private class ButtonHandler implements ActionListener {
-  // handle button event
-  @Override
-  public void actionPerformed(ActionEvent event) {
-    JOptionPane.showMessageDialog(LoginFrame.this, String.format("You pressed: %s", event.getActionCommand()));
+  private class ButtonHandler implements ActionListener {
+    // handle button event
+    @Override
+    public void actionPerformed(ActionEvent event) {
+      JOptionPane.showMessageDialog(LoginFrame.this,
+          String.format("You pressed: %s", event.getActionCommand()));
+    }
   }
+
 }
